afdbce07b9765b2fbbd06cd2afad570b61ed6c78
CenturionTheMan <dziedziakmichal0@gmail.com>
2024-03-02 21:38:43 +0100

    kk

--- DrawingIdentifierGui/Views/Controls/PredictionsCanvas.xaml.cs
+++ DrawingIdentifierGui/Views/Controls/PredictionsCanvas.xaml.cs
@@ -17,21 +17,8 @@ public partial class PredictionsCanvas : UserControl
     public PredictionsCanvas()
     {
         InitializeComponent();
+
     }
 
 
-
-    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-    {
-        System.Drawing.Color color = System.Drawing.Color.Black;
-
-        var bitmap = drawingCanvas
-            .GetBitmap()
-            .CropWhite()
-            .Resize(28, 28)
-            .ToBlackWhite(reverse: true);
-        bitmap.Save("D:\\GoogleDriveMirror\\Projects\\DrawingsIdentifier\\tmp1.png");
-
-      
-    }
 }
