6e708d0d807647b6ae42fcec12615c90718e831d
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2024-05-24 19:43:29 +0200

    config

--- DrawingIdentifierGui/ViewModels/Windows/FeedForwardConfigViewModel.cs
+++ DrawingIdentifierGui/ViewModels/Windows/FeedForwardConfigViewModel.cs
@@ -8,11 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using NeuralNetworkLibrary;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace DrawingIdentifierGui.ViewModels.Windows;
 
 public class FeedForwardConfigViewModel : ViewModelBase
 {
+    public RelayCommand AddLayerCommand => new RelayCommand((obj) =>
+    {
+        NNLayerConfig layer = new NNLayerConfig();
+        App.FeedForwardNNConfig.NeuralNetworkLayers!.Insert(App.FeedForwardNNConfig.NeuralNetworkLayers.Count()-1, layer);
+    });
+
+    public RelayCommand RemoveLayerCommand => new RelayCommand((obj) =>
+    {
+        if(SelectedLayer == null || SelectedLayer.IsSizeEnable == false || App.FeedForwardNNConfig.NeuralNetworkLayers!.Count <=3) return;
+
+        App.FeedForwardNNConfig.NeuralNetworkLayers!.Remove(SelectedLayer);
+    });
+
+
+    public NNLayerConfig SelectedLayer
+    {
+        get
+        {
+            return selectedLayer;
+        }
+        set
+        {
+            selectedLayer = value;
+            OnPropertyChanged();
+        }
+    }
+    private NNLayerConfig selectedLayer;
+
     public ObservableCollection<NNLayerConfig> NetworkLayers
     {
         get
