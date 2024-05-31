fd793b67b2b95c01d87ee8b9c536c566f4a4f839
unknown <dziedziakmichal0@gmail.com>
2024-05-31 13:41:10 +0200

    init

---
+++ NeuralNetworkLibrary/ConvolutionalNeuralNetwork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkLibrary;
+
+public class ConvolutionalNeuralNetwork : INeuralNetwork
+{
+    public Action<int, double, double>? OnLearningIteration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public bool IsStructureEqual(int[] layersSize, ActivationFunction[] activationFunctions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double[] Predict(double[] inputs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Train((double[] inputs, double[] outputs)[] data, double learningRate, int epochAmount, int batchSize, double expectedMaxError = 0.001)
+    {
+        throw new NotImplementedException();
+    }
+}
