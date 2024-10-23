ccfb02d79f7205ea8f56aa5167b52cf2ce84134f
Michał <64927330+CenturionTheMan@users.noreply.github.com>
2024-10-23 19:37:16 +0200

    project structure change

--- DrawingsIdentifier/NeuralNetworkLibrary/Math/MatrixHelpers.cs
+++ DrawingsIdentifier/NeuralNetworkLibrary/Math/MatrixHelpers.cs
@@ -2,8 +2,6 @@ namespace NeuralNetworkLibrary;
 
 static class MatrixHelpers
 {
-    private static Random random = new Random();
-
     public static int GetIndex(int row, int column, int columnsAmount)
     {
         return row * columnsAmount + column;
@@ -12,5 +10,5 @@ static class MatrixHelpers
     public static (int row, int column) GetRowAndColumn(int index, int columnsAmount)
     {
         return (index / columnsAmount, index % columnsAmount);
-    }
+    }    
 }
